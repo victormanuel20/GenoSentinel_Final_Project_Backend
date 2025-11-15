e6cb8b80ba0dab4e4b89adafea9c6c20af59aef1
victormanuel20 <victormanuel20034@gmail.com>
2025-11-15 14:02:44 -0500

    Inicio de proyecto

---
+++ autenticación/src/main/java/com/creators/autenticacion/AutenticacionApplication.java
@@ -0,0 +1,15 @@
+package com.creators.autenticacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacionApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(AutenticacionApplication.class, args);
+        System.out.println("Autenticacion Application started");
+
+	}
+
+}
---
+++ autenticación/src/test/java/com/creators/autenticacion/AutenticacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creators.autenticacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
