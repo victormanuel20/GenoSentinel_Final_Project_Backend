c2317b55ffac80a36a3a4e70b990a2d91e8a1e5e
victormanuel20 <victormanuel20034@gmail.com>
2025-11-29 02:41:16 -0500

    Excepciones funcionando para get

---
+++ autenticación/src/main/java/com/creators/autenticacion/exceptions/MicroserviceException.java
@@ -0,0 +1,18 @@
+package com.creators.autenticacion.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class MicroserviceException extends RuntimeException {
+
+    private final HttpStatus statusCode;
+    private final String errorMessage;
+
+    public MicroserviceException(HttpStatus statusCode, String errorMessage) {
+        super(errorMessage);
+        this.statusCode = statusCode;
+        this.errorMessage = errorMessage;
+    }
+
+}
